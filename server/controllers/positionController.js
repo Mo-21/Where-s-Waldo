6bf874e15143e5df2db1bf8e0f99d1840aa89a8c
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-19 01:10:16 +0300

    Add Try-Catch Block To Handle Post Requests

--- server/controllers/positionController.js
+++ server/controllers/positionController.js
@@ -1,5 +1,29 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
-exports.check_position = asyncHandler(async (req, res, next) => {
-  res.json("Position Received");
-});
+const Coordinates = require("../models/Coordinates");
+
+exports.check_position = [
+  body("x").isNumeric(),
+  body("y").isNumeric(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(500).json(errors.array());
+    } else {
+      try {
+        const x = await Coordinates.find(req.body.x).exec();
+        const y = await Coordinates.find(req.body.y).exec();
+
+        if (x && y) {
+          res.status(200).json("Incorrect!");
+        } else {
+          res.status(200).json("Correct! You Won");
+        }
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    }
+  }),
+];
