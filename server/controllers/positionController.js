f07224ec6c58b89999a2172aee7a7d4db0e14530
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-11-19 03:45:25 +0300

    Add DB Query to Find If The Click Is Correct

--- server/controllers/positionController.js
+++ server/controllers/positionController.js
@@ -2,24 +2,28 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 const Coordinates = require("../models/Coordinates");
+const { request } = require("express");
 
 exports.check_position = [
-  body("x").isNumeric(),
-  body("y").isNumeric(),
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
+
+    const result = await Coordinates.findOne({
+      name: req.body.name,
+      minX: { $lte: req.body.x },
+      maxX: { $gte: req.body.x },
+      minY: { $lte: req.body.y },
+      maxY: { $gte: req.body.y },
+    });
 
     if (!errors.isEmpty()) {
       res.status(500).json(errors.array());
     } else {
       try {
-        const x = await Coordinates.find(req.body.x).exec();
-        const y = await Coordinates.find(req.body.y).exec();
-
-        if (x && y) {
-          res.status(200).json("Incorrect!");
+        if (!result) {
+          res.status(200).json(false);
         } else {
-          res.status(200).json("Correct! You Won");
+          res.status(200).json(true);
         }
       } catch (error) {
         res.status(500).json(error);
